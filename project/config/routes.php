3d92f91d8deb35e5354057a3d7978fd14c28d9f5
EdwardDNB <edvarddnbs@gmail.com>
2023-03-09 18:35:33 +0200

    controller zad

--- project/config/routes.php
+++ project/config/routes.php
@@ -3,6 +3,9 @@
 	
 	return [
 		new Route('/hello/', 'hello', 'index'), // роут для приветственной страницы, можно удалить
+		new Route('/act1/', 'test', 'act1'),
+		new Route('/act2/', 'test', 'act2'),
+		new Route('/act3/', 'test', 'act3'),
 		new Route('/my-page1/', 'page', 'show1'),
 		new Route('/my-page2/', 'page', 'show2'),
 	];
